8e9d393464492ee1a693990cce34d26b934b58fd
Lalit Simariya <107851223+Lalitsimariya@users.noreply.github.com>
2026-06-27 17:23:40 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/Search Insert Position/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int pos=0;
+        for(int i=0;i<nums.length;i++){
+           
+            if(target==nums[i]){
+                pos=i;
+                return pos;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+           
+            if(target<nums[i]){
+                pos=i;
+                return pos;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+           
+            if(target>nums[i]){
+                pos=i+1;
+                
+            }
+        }
+        return pos;
+    }
+
+}
