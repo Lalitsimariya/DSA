52ce2be61563e28f9dc4cb3a682bf8dd6a21a99b
Lalit Simariya <107851223+Lalitsimariya@users.noreply.github.com>
2026-06-27 17:22:58 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/Divide Two Integers/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        double div=dividend,divi=divisor;
+        double c=div/divi;
+        int nums=(int) c;
+        System.out.println(nums);
+        System.out.println(c);
+        
+        return nums;
+    }
+}
