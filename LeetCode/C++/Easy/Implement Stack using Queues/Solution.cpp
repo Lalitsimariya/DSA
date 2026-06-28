e48811545101b6694b50144638bdc57399398548
Lalit Simariya <107851223+Lalitsimariya@users.noreply.github.com>
2026-06-28 19:59:20 +0530

    Add Easy C++ Solution from LeetCode

---
+++ LeetCode/C++/Easy/Implement Stack using Queues/Solution.cpp
@@ -0,0 +1,44 @@
+class MyStack {
+public:
+    int rear=-1;
+    int arr[6];
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        ++rear;
+        arr[rear]=x;
+
+
+    }
+    
+    int pop() {
+        int c=arr[rear];
+        --rear;
+        return c;
+
+    }
+    
+    int top() {
+        return arr[rear];
+    }
+    
+    bool empty() {
+        if(rear==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
