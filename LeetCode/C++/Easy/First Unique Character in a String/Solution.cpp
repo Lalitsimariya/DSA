5860544892922047ffe450377c2ccaabaed0ff7a
Lalit Simariya <107851223+Lalitsimariya@users.noreply.github.com>
2026-06-29 16:18:48 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/First Unique Character in a String/Solution.cpp
+++ LeetCode/C++/Easy/First Unique Character in a String/Solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        
+        vector<int>arr(s.length());
         int count=0;
         for(int i=0;i<s.length();i++){
             char c=s[i];
