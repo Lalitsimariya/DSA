ee8882ab377a9d3539c04945ca5f75540b27da63
Lalit Simariya <107851223+Lalitsimariya@users.noreply.github.com>
2026-06-29 16:56:32 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/First Unique Character in a String/Solution.cpp
+++ LeetCode/C++/Easy/First Unique Character in a String/Solution.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
+        int freq[26] = {0};
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s[i] - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
+
+
+//my approach
+/*
+class Solution {
+public:
+    int firstUniqChar(string s) {
         vector<int>arr;
         int count=0;
         int n = s.length();
-        for(int i=0;i<n;i++){
-            char c=s[i];
-            for(int j=0;j<n;j++){
-                if(c==s[j]){
-                    count++;
-                }
-            
-            }
+        
+        for(int j=0;j<n;j++){
+            char c=s[j];
+            count = c-'a';
             arr.push_back(count);
-            count=0;
-            
         }
+            
         for(int i=0;i<arr.size();i++){
             cout<<arr[i]<<" "<<i;
             if(arr[i]==1){
@@ -28,3 +45,5 @@ public:
         
     }
 };
+
+*/
