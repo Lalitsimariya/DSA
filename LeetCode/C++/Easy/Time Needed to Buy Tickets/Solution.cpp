f1339429b5da70c2d8a7de3d9fde66c7bddae1a7
Lalit Simariya <107851223+Lalitsimariya@users.noreply.github.com>
2026-06-30 16:08:40 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/Time Needed to Buy Tickets/Solution.cpp
+++ LeetCode/C++/Easy/Time Needed to Buy Tickets/Solution.cpp
@@ -5,16 +5,7 @@ public:
         int size=tickets.size();
         int start=0,count=0;
         for(int i=size-1;i>=0;i--){
-            int c=tickets[start]--;
-            if(c==0){
-                tickets.erase(tickets.begin());
-                count++;
-            }
-            else{
-                tickets.push_back(c);
-                count++;
-            }
-            start++;
+            
 
         }
         for(int i=size-1;i>=0;i--){
