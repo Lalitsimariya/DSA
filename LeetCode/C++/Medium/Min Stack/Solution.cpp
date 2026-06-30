7c6c912fad858bd7c97eb4efb4f9b8cebfdfe5e0
Lalit Simariya <107851223+Lalitsimariya@users.noreply.github.com>
2026-06-30 18:41:45 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/Min Stack/Solution.cpp
+++ LeetCode/C++/Medium/Min Stack/Solution.cpp
@@ -1,40 +1,41 @@
-#include<vector>
-#include<algorithm>
+#include <vector>
+
+
 class MinStack {
 public:
-    
     vector<int> arr;
-    MinStack() {
-        
-    }
-    
-    void push(int value) {
-        arr.push_back(value);
-    }
-    
-    void pop() {
-        int size=arr.size()-1;
-        arr.erase(arr.begin()+size);
-        
-    }
-    
-    int top() {
-        int high=arr.back();
-        return high;
-    }
-    
-    int getMin() {
-        int small=*min_element(arr.begin(),arr.end());
-        
-        return small;
+    vector<int> mini;
 
+    MinStack() {
+    }
+
+    void push(int val) {
+        arr.push_back(val);
+
+        if (mini.empty() || val <= mini.back())
+            mini.push_back(val);
+        else
+            mini.push_back(mini.back());
+    }
+
+    void pop() {
+        arr.pop_back();
+        mini.pop_back();
+    }
+
+    int top() {
+        return arr.back();
+    }
+
+    int getMin() {
+        return mini.back();
     }
 };
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
- * obj->push(value);
+ * obj->push(val);
  * obj->pop();
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
