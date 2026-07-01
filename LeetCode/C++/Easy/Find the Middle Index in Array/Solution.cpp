26917ba1e10707834f9dfb0102a85d73ef2963b4
Lalit Simariya <107851223+Lalitsimariya@users.noreply.github.com>
2026-07-01 16:07:43 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/Find the Middle Index in Array/Solution.cpp
+++ LeetCode/C++/Easy/Find the Middle Index in Array/Solution.cpp
@@ -1,13 +1,8 @@
-        int mid=nums.size();
-        else if(mid%2==0){
-            mid=mid/2;
-        }
-            return mid;
         else{
             mid++;
             mid=mid/2;
             return mid;
-    int findMiddleIndex(vector<int>& nums) {
-        if(mid==2){
-            return -1;
         }
+
+    }
+};
