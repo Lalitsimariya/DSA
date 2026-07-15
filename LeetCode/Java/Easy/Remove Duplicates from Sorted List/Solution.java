e84206caa9a072c6d37d04008509f60cf1267448
Lalit Simariya <107851223+Lalitsimariya@users.noreply.github.com>
2026-07-15 20:27:04 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/Remove Duplicates from Sorted List/Solution.java
+++ LeetCode/Java/Easy/Remove Duplicates from Sorted List/Solution.java
@@ -12,11 +12,14 @@ class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode temp=head;
         while(temp!=null&&temp.next!=null){
-            while()
+            
             if(temp.val==temp.next.val){
                 temp.next=temp.next.next;
             }
-            temp=temp.next;
+            else{
+
+                temp=temp.next;
+            }
         }
         return head;
     }
