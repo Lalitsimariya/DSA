f9f3ebe434a4116942126898601dee2cf64ddf6f
Lalit Simariya <107851223+Lalitsimariya@users.noreply.github.com>
2026-07-16 20:18:26 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/Move Zeroes/Solution.cpp
+++ LeetCode/C++/Easy/Move Zeroes/Solution.cpp
@@ -11,11 +11,11 @@ public:
                 }
                
             }
-            if(nums[i]>nums[i+1]){
+            
                     int temp=nums[i+1];
                     nums[i+1]=nums[i];
                     nums[i]=temp;
-                }
+                
             
         }
     }
