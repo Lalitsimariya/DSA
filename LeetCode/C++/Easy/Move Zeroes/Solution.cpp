95c3eaf826d78856b72458e50220ec39e86d9127
Lalit Simariya <107851223+Lalitsimariya@users.noreply.github.com>
2026-07-16 20:18:04 +0530

    Add Easy C++ Solution from LeetCode

---
+++ LeetCode/C++/Easy/Move Zeroes/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int size=nums.size();
+        for(int i=0;i<size-1;i++){
+            for(int j=0;j<size-1;j++){
+                if(nums[j]==0){
+                    int temp=nums[j+1];
+                    nums[j+1]=nums[j];
+                    nums[j]=temp;
+                }
+               
+            }
+            if(nums[i]>nums[i+1]){
+                    int temp=nums[i+1];
+                    nums[i+1]=nums[i];
+                    nums[i]=temp;
+                }
+            
+        }
+    }
+};
